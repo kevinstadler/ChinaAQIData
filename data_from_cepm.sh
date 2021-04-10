c07942ef8d01215f7079b2038b67acba8f6860f5
shared <lionandoil+github2@gmail.com>
2021-04-10 12:10:33 +0800

    Add explicit paths to bash script to make it work as a cron job

--- data_from_cepm.sh
+++ data_from_cepm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+cd `dirname ${BASH_SOURCE[0]}`
 
-wget http://106.37.208.233:20035/emcpublish/ClientBin/Env-CnemcPublish-RiaServices-EnvCnemcPublishDomainService.svc/binary/GetAQIDataPublishLives -O GetAQIDataPublishLives &&
+/usr/local/bin/wget --quiet http://106.37.208.233:20035/emcpublish/ClientBin/Env-CnemcPublish-RiaServices-EnvCnemcPublishDomainService.svc/binary/GetAQIDataPublishLives -O GetAQIDataPublishLives &&
+touch lastcheck
 
-python3 python-wcfbin/wcf2xml.py GetAQIDataPublishLives > data.xml &&
+/usr/local/bin/python3 python-wcfbin/wcf2xml.py GetAQIDataPublishLives > data.xml &&
 
 #cp data.xml xml/$(date +%Y%m%d%H%M)_data.xml
 
-python3 xml2json.py
+/usr/local/bin/python3 xml2json.py
 
 
